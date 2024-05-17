ed4d539b4ee29bdd01e0c80ed5afa41181aacb1e
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-05-17 11:36:28 +0530

    home screen sample design

--- lib/widget/normal_button.dart
+++ lib/widget/normal_button.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
 
+class NormalButton extends StatelessWidget {
+  const NormalButton({
+    super.key,
+    required this.buttonText,
+    required this.onPressed,
+  });
+
+  final String buttonText;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.black87,
+        minimumSize: const Size(double.infinity, 48),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      child: Text(
+        buttonText,
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w700,
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+}
